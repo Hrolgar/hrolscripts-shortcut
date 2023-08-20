c4d23b4f7188b4c38c972407eb66310aa2af763e
Helgi <helgiskafta@gmail.com>
2023-08-20 13:18:57 +0200

    Add git installation and repo cloning in install.ps1
    This commit adds PowerShell commands in the installation script (install.ps1) for automatic git installation (if not already installed) and cloning of a specific GitHub repository. The motivation for this change is to automate the setup of the project development environment, making it more straightforward and less error-prone for new developers. The script also appends the PowerShell profile file to auto-import the cloned module on the start of each PowerShell session.

---
+++ install.ps1
@@ -0,0 +1,30 @@
+﻿# Variables
+$repoUrl = "https://github.com/yourusername/yourrepository.git"
+$moduleDir = "HrolScripts"
+$moduleFile = "HrolScripts.psd1"
+$installLocation = Join-Path -Path $env:USERPROFILE -ChildPath "\Documents\_Development\PowerShell Modules\"
+$fullInstallPath = Join-Path -Path $installLocation -ChildPath "$moduleDir\$moduleFile"
+
+# Install git if not already installed
+if (!(Get-Command git -errorAction SilentlyContinue)) {
+    Write-Output "Installing Git..."
+    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    choco install git -y
+}
+
+# Clone the repository to the install location
+Write-Output "Cloning repository..."
+git clone $repoUrl $installLocation
+
+# Append the PowerShell profile file to auto import cloned module
+Write-Output "Adding module import to PowerShell profile..."
+if (!(Test-Path $PROFILE)) {
+    New-Item -Type file -Path $PROFILE -Force
+}
+$importCommand = "Import-Module `"$fullInstallPath`""
+if (!((Get-Content -Path $PROFILE) -match $importCommand))
+{
+    Add-Content -Path $PROFILE -Value $importCommand
+}
+
+Write-Output "Module installed! Please restart your PowerShell session."
